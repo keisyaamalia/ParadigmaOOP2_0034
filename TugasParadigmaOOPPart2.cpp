36822203937fe90fa74f61d849fca5cb2b3a5e75
keisyaamalia <kkaysaamalia@gmail.com>
2026-05-18 11:51:10 +0700

    create class RekeningBank and add pure virtual function

--- TugasParadigmaOOPPart2.cpp
+++ TugasParadigmaOOPPart2.cpp
@@ -1,2 +1,22 @@
 #include <iostream>
 using namespace std;
+
+class RekeningBank
+{
+protected:
+    string nomorRekening;
+    double saldo;
+
+public:
+    RekeningBank(string no, double saldoAwal)
+        : nomorRekening(no), saldo(saldoAwal) {}
+
+    virtual void potongAdmin() = 0;
+
+    void tampilkanInfo() {
+        cout << "No Rek : " << nomorRekening
+             << " | Saldo Akhir : Rp " << saldo << endl;
+    }
+
+    virtual ~RekeningBank() {}
+};
