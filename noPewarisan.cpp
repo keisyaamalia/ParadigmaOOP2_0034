29c9859d3a929647112075685f2e7a1a41d56c6c
keisyaamalia <kkaysaamalia@gmail.com>
2026-05-18 11:00:27 +0700

    create class derivedClass and access modifier

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -16,3 +16,10 @@ public:
         cout << "Hallo saya Function dari base class";
     }
 };
+
+class derivedClass : public baseClass1 {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
